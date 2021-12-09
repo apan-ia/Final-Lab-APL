a82e04ff9f6252d048a64f8eda108572ff929372
apan-ia <82051942+apan-ia@users.noreply.github.com>
2021-12-09 23:20:10 +0700

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -2,6 +2,6 @@ public class Rectangle implements Shape{
     //implementasi method dari interface Shape
     @Override
     public void draw() {
-        System.out.println("Drawing Rectangle");
+        System.out.println("\n\n\nDrawing Rectangle");
     }
 }
